12c56305abda18bcb5ab2e86472fae0bb945419a
d3c6e1 <p0nede1niq@gmail.com>
2020-05-12 00:23:04 +0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Client());
+}
+
+class Client extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
